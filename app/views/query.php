5bb9c4c13aff5977da544a173cee272a912a102d
Aaron Cicali <aaron.cicali@gmail.com>
2016-08-09 06:57:39 -0400

    removes unnecessary element that was breaking the style of raw query window

--- app/views/query.php
+++ app/views/query.php
@@ -1,13 +1,11 @@
-<div id='query'>
-    <form action='<?php echo Params::toString(); ?>' method='post'>
-        <fieldset>
-            <?php if(empty($schema['database'])): ?>
-            <legend>Run a query on server "<?php echo Configs::get('host'); ?>":</legend>
-            <?php else :?>
-            <legend>Run a query on database <a href='/?database=<?php echo $schema['database']; ?>'><?php echo $schema['database']; ?></a>:</legend>
-            <?php endif; ?>
-            <textarea id='query-textarea' name='query'></textarea>
-            <input type='submit' value='Go'/>
-        </fieldset>
-    </form>
-</div>
+<form action='<?php echo Params::toString(); ?>' method='post'>
+    <fieldset>
+        <?php if(empty($schema['database'])): ?>
+        <legend>Run a query on server "<?php echo Configs::get('host'); ?>":</legend>
+        <?php else :?>
+        <legend>Run a query on database <a href='/?database=<?php echo $schema['database']; ?>'><?php echo $schema['database']; ?></a>:</legend>
+        <?php endif; ?>
+        <textarea id='query-textarea' name='query'></textarea>
+        <input type='submit' value='Go'/>
+    </fieldset>
+</form>

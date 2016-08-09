cb5ccc03d7aa9dc9c3bc70d46400fd7343a6fdc2
Aaron Cicali <aaron.cicali@gmail.com>
2016-08-09 06:44:35 -0400

    implements search clauses, adds joins

--- app/views/search.php
+++ app/views/search.php
@@ -2,36 +2,45 @@
     <?php if(empty($database) OR empty($columns)): ?>
     <div class='error'>No database and table selected</div>
     <?php else :?>
+    <?php
+        $action = Params
+            ::only('database', 'table')
+            ->with('route', 'query')
+            ->toString();
+    ?>
+    <form action='<?php echo $action; ?>' method='post'>
         <fieldset>
-            <legend>Do a "query by example" (wildcard: "%")</legend>
+            <legend>Run a query on server "<?php echo Configs::get('host'); ?>"</legend>
             <table class='striped'>
-                <tr>
-                    <th colspan='6' style='text-align: left;'>SELECT <span id='selected-fields'>*</span></th>
-                </tr>
                 <tr>
                     <th></th>
                     <th>Field</th>
                     <th>Type</th>
-                    <th>Collation</th>
                     <th>Operator</th>
                     <th>Value</th>
+                    <th>JOIN</th>
+                    <th>ON</th>
                 </tr>
             <?php foreach($columns as $column): ?>
-                <tr class='selectable'>
+                <tr class='selectable searchable-column'>
                     <td><input type='checkbox' name='select[]' value='<?php echo $column['field']; ?>'/></td>
                     <td class='bold'><?php echo $column['field']; ?></td>
                     <td><?php echo $column['type']; ?></td>
-                    <td><?php echo $column['collation']; ?></td>
                     <td>
-                        <select name='func[]' class='no-select'>
-                            <option value='='>=</option>
-                            <option value='>'>&gt;</option>
-                            <option value='>='>&gt;=</option>
-                            <option value='<'>&lt;</option>
-                            <option value='<='>&lt;=</option>
-                            <option value='!='>!=</option>
-                            <option value='LIKE'>LIKE</option>
-                            <option value='NOT LIKE'>NOT LIKE</option>
+                        <select name='operator[]' role='operator' class='no-select'>
+                            <option value='= "{{value}}"'>=</option>
+                            <option value='> "{{value}}"'>&gt;</option>
+                            <option value='>= "{{value}}"'>&gt;=</option>
+                            <option value='< "{{value}}"'>&lt;</option>
+                            <option value='<= "{{value}}"'>&lt;=</option>
+                            <option value='!= "{{value}}"'>!=</option>
+                            <option value='= ""'>= ''</option>
+                            <option value='!= ""'>!= ''</option>
+                            <option value='REGEXP "{{value}}"'>REGEXP</option>
+                            <option value='NOT REGEXP "{{value}}"'>NOT REGEXP</option>
+                            <option value='LIKE "{{value}}"'>LIKE</option>
+                            <option value='LIKE "%{{value}}%"'>LIKE %...%</option>
+                            <option value='NOT LIKE "{{value}}"'>NOT LIKE</option>
                             <option value='IS NULL'>IS NULL</option>
                             <option value='IS NOT NULL'>IS NOT NULL</option>
                         </select>
@@ -39,24 +48,92 @@
                     <td>
                         <input
                             name='<?php echo $column['field']; ?>'
+                            role='value'
                             autocomplete='off'
                             class='no-select'/>
                     </td>
+                    <td>
+                        <select role='join-type' class='no-select'>
+                            <option>LEFT</option>
+                            <option>RIGHT</option>
+                            <option>INNER</option>
+                            <option>FULL OUTER</option>
+                        </select>
+                        <select role='join' class='no-select'>
+                            <option></option>
+                        <?php foreach($database->tables() as $joinable): ?>
+                            <?php
+                                if($joinable->name == $table->name){
+                                    continue;
+                                }
+                                $joinable_columns = array();
+                                foreach($joinable->columns() as $joinable_column){
+                                    $joinable_columns[] = $joinable_column['field'];
+                                }
+                                $joinable_columns = implode(',', $joinable_columns);
+                            ?>
+                            <option
+                                value='<?php echo $joinable->name; ?>'
+                                columns='<?php echo $joinable_columns; ?>'>
+                                <?php echo $joinable->name; ?>
+                            </option>
+                        <?php endforeach; ?>
+                        </select>
+                    </td>
+                    <td>
+                        <select role='join-on' class='no-select'></select>
+                    </td>
                 </tr>
             <?php endforeach; ?>
+                <tr>
+                    <td colspan='8'>
+                        <textarea name='query' id='query' from='<?php echo Params::get('table'); ?>'></textarea>
+                    </td>
+                </tr>
             </table>
         </fieldset>
-        <?php
-        $action =
-            Params::only('database', 'table')
-                ->with('route', 'query')
-                ->toString();
-        ?>
-        <form action='<?php echo $action; ?>' method='post'>
-            <input type='hidden' name='query' id='query' from='<?php echo Params::get('table'); ?>'/>
-            <input type='submit' value='Go'/>
-        </form>
+        <input type='submit' value='Go'/>
+    </form>
     <?php endif; ?>
 </div>
+<div class='venn-diagram'>
+    <div class='circle left-circle'></div>
+    <div class='circle right-circle'>
+        <div class='circle center-circle'></div>
+    </div>
+</div>
+<style>
+.venn-diagram {
+    position: relative;
+    float: left;
+    width: 120px;
+    height: 120px;
+    margin: 5px 5px 0 0;
+}
 
-<h1>Clauses not yet implemented</h1>
+.circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 75%;
+    height: 75%;
+    overflow: hidden;
+    background: #FFF;
+    border-radius: 500px;
+    box-shadow: 0 0 0 1px #000;
+}
+
+.circle:hover {
+    background: #000;
+}
+
+.right-circle {
+    margin-left: 50%;
+}
+
+.center-circle {
+    width: 100%;
+    height: 100%;
+    margin-left: -66.6666666%;
+}
+</style>
--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,35 +1,98 @@
-var query = {
-        select: '*',
-        from: $('[from]').attr('from'),
-        where: []
-};
+var $search = $('#search');
+var $columns = $search.find('.searchable-column');
 var $query = $('#query').val(renderQuery());
-var $selected = $('#selected-fields')
-var $search = $('#search')
-    .on('change', ':checkbox', function(){
-        var select = [];
-        var $checked = $search.find(':checkbox').filter(':checked');
 
-        $checked
-            .each(function(index){
-                select.push('`' + $checked.eq(index).val() + '`');
-            });
-
-        select = select.length
-            ? select.join(', ')
-            : '*';
-
-        query.select = select;
-        $selected.text(select);
+$search
+    .on('change keyup', 'input, select', function(){
+        populateOnClauses();
         $query.val(renderQuery());
     });
 
+function renderSelect(){
+    var table = getTableName();
+    var select = [];
+    var $checked = $search.find(':checkbox').filter(':checked');
+    $checked
+        .each(function(index){
+            select.push('\n`' + table + '`.`' + $checked.eq(index).val() + '`');
+        });
+    return select.length
+        ? select.join(',')
+        : ' *';
+}
+
+function getTableName(){
+    return $('[from]').attr('from');
+}
+
+function renderJoins(){
+    var joins = [];
+    var table = getTableName();
+    $columns.each(function(index){
+        var $column = $columns.eq(index);
+        var type = $column.find('[role=join-type]').val();
+        var join = $column.find('[role=join]').val()
+        var on = $column.find('[role=join-on]').val();
+        if(join.length && on){
+            var column = $column.find('[role=value]').attr('name');
+            joins.push(
+                '\n' + type + ' JOIN `' +
+                join +
+                '`\n\tON `' + join + '`.`' + on +
+                '` = `' + table + '`.`' + column + '`'
+            );
+        }
+    });
+    return joins;
+}
+
+function renderWhere(){
+    var where = [];
+    $columns.each(function(index){
+        var $column = $columns.eq(index);
+        var $value = $column.find('[role=value]');
+        var operator = $column.find('[role=operator]').val();
+        var field = '`' + $value.attr('name') + '` ';
+        var value = $value.val();
+        if(operator.indexOf('{{value}}') === false){
+            where.push(field + operator);
+        }
+        else if(value.length){
+            where.push(field + operator.replace('{{value}}', value));
+        }
+    });
+    return where.length
+        ? where.join(' AND ')
+        : 1;
+}
+
 function renderQuery(){
+    populateOnClauses();
+    var table = getTableName();
     return [
-        'SELECT ',
-        query.select,
-        ' FROM `',
-        query.from,
-        '` '
+        'SELECT', renderSelect(),
+        '\nFROM `', table, '`',
+        renderJoins(),
+        '\nWHERE ', renderWhere()
     ].join('');
+}
+
+function populateOnClauses(){
+    $columns.each(function(index){
+        var $column = $columns.eq(index);
+        var columns = $column
+                .find('[role=join]')
+                .find('option:selected')
+                .attr('columns') || '';
+        $column
+            .find('[role=join-on]')
+            .html(
+                '<option>' +
+                columns
+                    .split(',')
+                    .join('</option><option>') +
+                '</option>'
+            );
+
+    });
 }
